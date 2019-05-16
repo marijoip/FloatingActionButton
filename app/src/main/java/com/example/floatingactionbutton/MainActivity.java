e80deb39ca20d33339bca342511931700b810b01
MARIA <majoibarra1022@gmail.com>
2019-05-16 15:20:29 -0500

    MainActivity.java - Mensaje "Se presionó el FAB"                                                            activity_main.xml - Boton Compartir (Rompimiento de bordes)                                                           MainActivity.java - Animación de Escala en el Floating Action
    MainActivity.java - Rotar floating action botton al clickearlo

--- app/src/main/java/com/example/floatingactionbutton/MainActivity.java
+++ app/src/main/java/com/example/floatingactionbutton/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.floatingactionbutton;
 
 
-import android.animation.Animator;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -12,7 +10,9 @@ import android.view.animation.AnimationUtils;
 import android.view.animation.Interpolator;
 
 public class MainActivity extends AppCompatActivity {
-       //Animación escala en el floating action button
+        //Rotar floating action btton al clickearlo
+    boolean click = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,52 +20,51 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setScaleX(0);
-        fab.setScaleY(0);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setImageResource(R.drawable.ahsi);
 
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-            final Interpolator interpolador = AnimationUtils.loadInterpolator(getBaseContext(),
-                    android.R.interpolator.fast_out_slow_in);
-
-            fab.animate()
-                    .scaleX(1)
-                    .scaleY(1)
-                    .setInterpolator(interpolador)
-                    .setDuration(600)
-                    .setStartDelay(1000)
-                    .setListener(new Animator.AnimatorListener() {
-                        @Override
-                        public void onAnimationStart(Animator animation) {
-
-                        }
-
-                        @Override
-                        public void onAnimationEnd(Animator animation) {
-                            fab.animate()
-                                    .scaleY(0)
-                                    .scaleX(0)
-                                    .setInterpolator(interpolador)
-                                    .setDuration(600)
-                                    .start();
-                        }
-
-                        @Override
-                        public void onAnimationCancel(Animator animation) {
-
-                        }
-
-                        @Override
-                        public void onAnimationRepeat(Animator animation) {
-
-                        }
-                    });
-        }
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Se presionó el FAB", Snackbar.LENGTH_LONG).show();
+                click = !click;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    Interpolator interpolador = AnimationUtils.loadInterpolator(getBaseContext(),
+                            android.R.interpolator.fast_out_slow_in);
+
+                    view.animate()
+                            .rotation(click ? 45f : 0)
+                            .setInterpolator(interpolador)
+                            .start();
+                }
             }
         });
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
